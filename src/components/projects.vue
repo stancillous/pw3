af8a34501019d0e8ee39bc81b35b75be1f0b7da8
Stancillous Raymond <stancillousray@gmail.com>
2022-10-21 14:17:59 +0300

    updated spotify wrapper's github link

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -13,10 +13,10 @@
                     <!-- SPOTIFY PROJECT -->
                     <div class="project-info">
                         <div class="project-section-icons-container">
-                            <a  target="_blank" class="code" href="https://github.com/stancillous/spotify-vue-app"><i class="fa-solid fa-code"></i></a>
+                            <a  target="_blank" class="code" href="https://github.com/stancillous/spotify-wrapper-v4"><i class="fa-solid fa-code"></i></a>
 
                             <div class="github-and-external-link">
-                                <a  target="_blank" href="https://spotify-wrapper.netlify.app/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                                <a  target="_blank" href="https://spotify-wrapper.netlify.app/login.html"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
                             </div>
                         </div>   
 
