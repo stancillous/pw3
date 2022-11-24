cf08e0c62186034ffabe7400b5874b79b32768ee
Stancillous Raymond <stancillousray@gmail.com>
2022-11-24 23:22:38 +0300

    Update projects.vue

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -136,27 +136,23 @@
                         </div>
 
                     </div>
+                    
 
                     <div class="project-info">
 
                            <div class="project-section-icons-container">
-                            <a  target="_blank" class="code" href="https://github.com/stancillous/weather-app">
-                               <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg> 
-                            </a>
+                            <a  target="_blank" class="code" href="https://github.com/stancillous/cyHealth"><i class="fa-solid fa-code"></i></a>
 
                             <div class="github-and-external-link">
-                                <a  target="_blank" href="https://w-info.netlify.app/">
-                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
-                                    </svg>
-                                </a>
+                                <a  target="_blank" href="https://cyhealth.netlify.app/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
                             </div>
                         </div>   
 
-                     <h1>weather web app</h1>
-                     <p>A simple weather app. I was interested in learning to work with APIs so I found a well documented one and developed this web app.</p>
+                     <h1>API connected app</h1>
+                     <p>A web app that shows covid-19 analysis for any country. Filterable by countries.</p>
                      <div class="tech-used-container">
                          <span class="tech-used">Vue.js</span>
-                         <span class="tech-used">API</span>
+                         <span class="tech-used">REST API</span>
                      </div>
 
                     </div>
@@ -277,11 +273,12 @@
                             </div>
                         </div>   
 
-                     <h1>weather web app</h1>
+                     <h1>RESTful API app</h1>
+                     <p>A web app that shows covid-19 analysis for any country. Filterable by countries.</p>
                      <p>A simple weather app. I was interested in learning to work with APIs so I found a well documented one and developed this web app.</p>
                      <div class="tech-used-container">
                          <span class="tech-used">Vue.js</span>
-                         <span class="tech-used">API</span>
+                         <span class="tech-used">REST API</span>
                      </div>
 
                     </div>
