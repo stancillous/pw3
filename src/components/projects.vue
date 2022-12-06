dd319108c0f28f5877afe17b12498238992064b2
Stancillous Raymond <stancillousray@gmail.com>
2022-12-06 10:38:58 +0300

    Update projects.vue

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -36,29 +36,29 @@
                     
                     <div class="project-info">
                         <div class="project-section-icons-container">
-                            <a target="_blank" class="code" href="https://github.com/stancillous/C-Profile">
-                               <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                            </svg> 
+                            <a  target="_blank" class="code" href="https://github.com/stancillous/exceed-shop">
+                               <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg> 
                             </a>
 
                             <div class="github-and-external-link">
-                                <a target="_blank"  href="https://c-profiles.netlify.app/">
+                                <a  target="_blank" href="https://exceed-shop.netlify.app/">
                                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
                                     </svg>
                                 </a>
                             </div>
                         </div>   
 
-                        <h1>C-Profiles</h1>
-                        <p>A simpler and quicker way for github users to view their repository stats, languages used for each repo.Includes chart visualizations of your top languages.</p>
+                        <h1>Exceed online shop</h1>
+                        <p>A website concept that I am currently working on, for an online apparel shop. Built with Nuxt.js</p>
                         <div class="tech-used-container">
-                            <span class="tech-used">Github API</span>
-                            <span class="tech-used">Chart.js</span>
-
+                            <span class="tech-used">Nuxt.js</span>
+                            <span class="tech-used">TypeScript</span>
+          
                         </div>
 
                     </div>
-
+                    
+    
                     
                     <div class="project-info">
                         <div class="project-section-icons-container">
@@ -87,26 +87,29 @@
                     </div>
                     
                     
+                    
+                    
                     <div class="project-info">
                         <div class="project-section-icons-container">
-                            <a  target="_blank" class="code" href="https://github.com/stancillous/exceed-shop">
-                               <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg> 
+                            <a target="_blank" class="code" href="https://github.com/stancillous/C-Profile">
+                               <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                            </svg> 
                             </a>
 
                             <div class="github-and-external-link">
-                                <a  target="_blank" href="https://exceed-shop.netlify.app/">
+                                <a target="_blank"  href="https://c-profiles.netlify.app/">
                                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
                                     </svg>
                                 </a>
                             </div>
                         </div>   
 
-                        <h1>Exceed online shop</h1>
-                        <p>A website concept that I am currently working on, for an online apparel shop. Built with Nuxt.js</p>
+                        <h1>C-Profiles</h1>
+                        <p>A simpler and quicker way for github users to view their repository stats, languages used for each repo.Includes chart visualizations of your top languages.</p>
                         <div class="tech-used-container">
-                            <span class="tech-used">Nuxt.js</span>
-                            <span class="tech-used">TypeScript</span>
-          
+                            <span class="tech-used">Github API</span>
+                            <span class="tech-used">Chart.js</span>
+
                         </div>
 
                     </div>
